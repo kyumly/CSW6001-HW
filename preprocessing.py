7a5c965a725a069e86ce3c0dfedc0b30009336ec
kyumly <skkim3360@naver.com>
2024-04-17 10:25:19 +0900

    init

---
+++ preprocessing.py
@@ -0,0 +1,67 @@
+import numpy
+import numpy as np
+
+def get_scatter_matrix(X):
+    """
+    Scatter matrix 구현하는 함수
+    """
+    X : numpy.array
+    N = X.shape[0]
+    mean = X.mean(axis = 0)
+    X = X - mean
+    # numpy.cov(X, rowvar = False, bias=True)
+    result = (X.T @ X)
+    return result
+
+
+def get_eigen_vectors(X):
+    eigen_value, eigen_vector = numpy.linalg.eig(X)
+    # eigen_value = np.diag(eigen_va/lue)
+    index = np.argsort(eigen_value)[::-1]
+    eigen_value = eigen_value[index]
+    eigen_vector = eigen_vector[:, index]
+
+    return eigen_value, eigen_vector
+
+class Linear():
+    """
+    projection 투영하는 클래스
+    """
+    def __init__(self, X=None, vector=None, value=None):
+        self.X = X
+        self.vector = vector
+        self.value = value
+        self.N, self.D =X.shape
+
+    def forward(self, X):
+        return self.X @ self.vector
+
+    def __call__(self, num=1):
+        return self.forward(num)
+
+    def get_mean(self, X):
+        mean = X.mean(axis=0)
+        return mean
+
+class PCA(Linear):
+    """
+    PCA 구현하는 클래스
+    """
+    def __init__(self, X, vector, value):
+        super(PCA, self).__init__(X, vector, value)
+        self.explained_variance_ratio = value / value.sum()
+
+    def forward(self, num=1):
+        # return self.X @ self.vector
+        X = self.X - self.get_mean(self.X)
+        return X @ self.vector[:, range(num)]
+        # if num > self.D : raise ValueError("rank of X must be")
+        # data = super().forward(X)
+        # return data[:, range(num)]
+
+class LDA():
+    """
+    LDA 구현하는 클래스
+    """
+    def __init__(self):
+        pass
