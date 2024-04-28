af7c524d5caa28155caea3a979cd5de1afce45da
kyumly <skkim3360@naver.com>
2024-04-28 19:55:02 +0900

    정리 진행중

--- model.py
+++ model.py
@@ -1,32 +1,135 @@
 import numpy as np
+import statistics
+import matplotlib.pyplot as plt
+np.random.seed(42)
+
+
+
+def knn_cross_validate( x_train, y_train, num_folds: int = 5, k_choices = [1, 3, 5, 8, 10, 12, 15, 20, 50, 100]):
+
+    # x_train과 y_train을 랜덤하게 섞기
+
+    x_train_folds = np.array_split(x_train, num_folds, axis=0)
+    y_train_folds = np.array_split(y_train, num_folds, axis=0)
+    k_to_accuracies = {}
+
+    for k in k_choices:
+        accuracy = []
+
+        for i in range(num_folds):
+            validation_x = x_train_folds[i]
+            validation_y = y_train_folds[i]
+            train_x = x_train_folds[:i] + x_train_folds[i+1:]
+            train_y = y_train_folds[:i] + y_train_folds[i+1:]
+
+            train_x = np.concatenate(train_x, axis=0)
+            train_y = np.concatenate(train_y, axis=0)
+            
+            knn = KnnClassifier(train_x, train_y)
+            accuracy.append(knn.check_accuracy(validation_x, validation_y, k,True))
+
+        k_to_accuracies[k] = accuracy
+    return k_to_accuracies
+
+
+def grid_cross_validation(k_to_accuracies, name, pca, i):
+    ks, means, stds = [], [], []
+    for k, accs in sorted(k_to_accuracies.items()):
+        plt.scatter([k] * len(accs), accs, color='g')
+        ks.append(k)
+        means.append(statistics.mean(accs))
+        stds.append(statistics.stdev(accs))
+    plt.errorbar(ks, means, yerr=stds)
+    plt.xlabel('k')
+    plt.ylabel('Cross-validation accuracy')
+    plt.title(f'Cross-validation on {name} {pca}')
+    plt.savefig(f"./fig/{i}.png")
+    plt.show()
+
+
+def confusion_matrix(actual, predicted, labels):
+    num_labels = len(labels)
+    matrix = np.zeros((num_labels, num_labels))
+    # 실제 클래스와 예측한 클래스 간의 관계를 계산하여 Confusion matrix 작성
+    for a, p in zip(actual, predicted):
+        a_index = labels.index(a)
+        p_index = labels.index(p)
+        matrix[a_index, p_index] += 1
+
+    return matrix
+
 
 class KnnClassifier:
     def __init__(self, x_train, y_train):
         self.x_train = x_train
         self.y_train = y_train
+        self.classes = list(set(y_train))
 
-    def predict(self, x_test, k: int = 1):
-
-        N = self.x_train.shape[0]
-        N_t = x_test.shape[0]
-
-        train = self.x_train.reshape(N, -1)
-
-        test = x_test.reshape(N_t, -1)
-
-        train_sum = np.sum((train ** 2), dim=1).reshape(-1, 1)
-        test_sum = np.sum((test ** 2), dim=1)
-
-        train_test_mm = -2 * np.mm(train, test.T)
-
-        dists = (train_sum + test_sum + train_test_mm) ** (1 / 2)
-
+    def predict2(self, x_test, k: int = 1):
+        dists = self.get_distance(x_test)
         num_train, num_test = dists.shape
+
         y_pred = np.zeros(num_test)
 
         for i in range(num_test):
             label = np.argsort(dists[:, i])
-            closest_y = np[label[:k]]
+            closest_y = self.y_train[label[:k]]
             y_pred[i] = np.bincount(closest_y).argmax()
+        return y_pred
+
+
+
+    def get_distance(self, x_test):
+        num_train = self.x_train.shape[0]
+        num_test = x_test.shape[0]
+        dists = np.zeros((num_train, num_test))
+        for i in range(num_train):
+            distint = np.sqrt(((self.x_train[i] - x_test) ** 2).sum(axis=1))
+            dists[i, :] = distint
+
+        return dists
+
+
+    def predict(self, X_test, k=1):
+        y_pred = np.zeros(X_test.shape[0])
+
+        for i, x in enumerate(X_test):
+            distances = np.sqrt(np.sum((self.x_train - x) ** 2, axis=1))
+            nearest_indices = distances.argsort()[:k]
+            nearest_labels = self.y_train[nearest_indices]
+            y_pred[i] = np.bincount(nearest_labels).argmax()
 
         return y_pred
+
+
+    def check_accuracy( self, x_test, y_test, k: int = 1, quiet: bool = False ):
+        y_test_pred = self.predict(x_test, k=k)
+        num_samples = x_test.shape[0]
+        num_correct = (y_test == y_test_pred).sum()
+
+        accuracy = 100.0 * num_correct / num_samples
+        msg = (
+            f"Got {num_correct} / {num_samples} correct; "
+            f"accuracy is {accuracy:.2f}%"
+        )
+        if not quiet:
+            print(msg)
+        return accuracy
+
+
+def knn_get_best_k(k_to_accuracies, axis_choices):
+    max_accuracy = 0
+    axis = 0
+    best_k= 0
+    for index, accuracy in enumerate(k_to_accuracies):
+        for k, accuracies in accuracy.items():
+            max_acc_for_k = max(accuracies)  # 각 키에 해당하는 값들의 리스트에서 최대값 찾기
+            if max_acc_for_k > max_accuracy:
+                max_accuracy = max_acc_for_k
+                best_k = k
+                axis = axis_choices[index]
+
+    print("Best K:", best_k)
+    print("Best axis : ", axis)
+    print("Max Accuracy:", max_accuracy)
+    return best_k, axis
--- preprocessing.py
+++ preprocessing.py
@@ -3,9 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-COLOR = ["r", "g", 'b']
 
 
+def  get_angle_in_degrees(vector1, vector2):
+    """
+    서로의 각도 구하는 식
+    """
+    cos_sim = np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2)) 
+    truncated_arr = np.round(np.trunc(cos_sim * 100) / 100, 1)
+    # 코사인 유사도를 각도로 변환
+    angle_in_radians = np.arccos(truncated_arr)
+    angle_in_degrees = np.degrees(angle_in_radians)
+    return angle_in_degrees
 
 def get_eigen_vectors(X):
     eigen_value, eigen_vector = numpy.linalg.eig(X)
@@ -14,6 +23,31 @@ def get_eigen_vectors(X):
     eigen_value = eigen_value[index]
     eigen_vector = eigen_vector[:, index]
     return eigen_value, eigen_vector
+
+
+def grid_3d(test_reuslt, y_test, name, label_encode_dict):
+    # 라벨별로 데이터를 모으기
+    unique_labels = np.unique(y_test)
+    label_data = {label: [] for label in unique_labels}
+    for i, label in enumerate(y_test):
+        label_data[label].append(test_reuslt[i])
+
+    # 3D 플롯 그리기
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # 각 라벨별로 데이터를 플롯에 추가
+    for label, data_points in label_data.items():
+        data_points = np.array(data_points)
+        ax.scatter(data_points[:,0], data_points[:,1], data_points[:,2], label=f'{label_encode_dict[label]}')
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    ax.legend()
+    plt.savefig(f"./fig/{name} grid_3d.png")
+    plt.show()
+
 
 class PCA():
     """
@@ -41,9 +75,28 @@ class PCA():
            v = v.reshape(-1 ,1)
         self.v = v
 
+    def reconstruct(self, X = None):
+        """
+        예시 :
+        벡터 (2,1) @ (1,)
+        reconst_test = (test_Vector.T @ valuess + 5)
+
+        :param num:
+        :return:
+        """
+        if X is not None:
+            mean = self.get_mean(X)
+            X = X - mean
+            return (X @ self.v) @ self.v.T + mean
+
+        mean = self.get_mean(self.X)
+        X = self.X - mean
+        return (X @ self.v) @ self.v.T + mean
+
     def get_mean(self, X):
         mean = X.mean(axis=0)
         return mean
+
     def get_scatter_matrix(self):
         """
         Scatter matrix 구현하는 함수
@@ -55,20 +108,10 @@ class PCA():
         result = (X.T @ X) / self.N
         return result
 
-    def reconstruct(self):
-        """
-        예시 :
-        벡터 (2,1) @ (1,)
-        reconst_test = (test_Vector.T @ valuess + 5)
-
-        :param num:
-        :return:
-        """
-        mean = self.get_mean(self.X)
-        X = self.X - mean
-        return (X @ self.v) @ self.v.T + mean
-
-    def __call__(self):
+    def __call__(self, X =None):
+        if X is not None:
+            X = X - self.get_mean(X)
+            return X @ self.v
         X = self.X - self.get_mean(self.X)
         return X @ self.v
 
@@ -80,6 +123,7 @@ class LDA():
     def __init__(self, data, target):
         super().__init__()
         #입력 데이터 정보 저장
+        
         self.data = data
         self.N, self.F = data.shape
         
@@ -87,13 +131,56 @@ class LDA():
         self.classes = np.unique(target)
         self.labels = target
         self.vector = None
+    
+    def fit_svd(self, num=1):
+        _, y_t = np.unique(self.labels, return_inverse=True)  #
+        
+        priors_ = np.bincount(y_t) / float(len(self.labels))
 
 
-    def fit(self, num=1):
-        # print(self.data[self.labels == 0] - self.data[self.labels == 0].mean(axis=0))
+        XG = []
+
+        n_classes = len(list(self.classes))
+
+        mean_list = [self.data[self.labels == c].mean(axis=0) for c in self.classes]
+        mean_stack = np.stack(mean_list)
+
+        for idx, g in enumerate(self.classes):
+            xg = self.data[self.labels == g]
+            XG.append(xg - mean_list[idx])
+        
+        xbar_ = np.dot(priors_, mean_stack)    
+        mean_data = np.concatenate(XG, axis=0)
+
+        std = mean_data.std(axis=0)
+        std[std == 0] = 1.0
+        fac = 1.0 / (self.N - n_classes)
+        
+        X = np.sqrt(fac) * (mean_data / std)
+        U, S, Vt = np.linalg.svd(X, full_matrices=False)
+        rank = np.sum(S > 0.0001)
+
+        scalings = (Vt[:rank] / std).T / S[:rank]
+        fac = 1.0 if n_classes == 1 else 1.0 / (n_classes - 1)
+
+        X = np.dot(
+            (
+                (np.sqrt((self.N *  priors_) * fac))
+                * (mean_stack -  xbar_).T
+            ).T,
+            scalings,
+        )
+
+        _, S, Vt = np.linalg.svd(X, full_matrices=0)
+        rank = np.sum(S > 0.0001 * S[0])
+        self.vector = np.dot(scalings, Vt.T[:, :num])
+
+
+    def fit_eigen(self, num=1):
         mean_list = [self.data[self.labels == c].mean(axis=0) for c in self.classes]
         SW, SB = self.get_scatter_matrix(mean_list)
-        inverse = np.linalg.inv(SW).dot(SB)
+
+        inverse = np.linalg.pinv(SW).dot(SB)
         value, vector = get_eigen_vectors(inverse)
         vector = vector[:, :num].real
         self.vector = vector
@@ -114,6 +201,7 @@ class LDA():
         return S_W, S_B
 
 
-    def __call__(self):
+    def __call__(self, X = None):
+        if X is not None:
+            return X @ self.vector
         return  self.data @ self.vector
-
