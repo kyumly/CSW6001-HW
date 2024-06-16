a7ab7b4f090a89376ad512ae9ee10ebf76a2bb67
kyumly <skkim3360@naver.com>
2024-06-16 19:58:07 +0900

    SVM 완료

--- HW2/SVM.py
+++ HW2/SVM.py
@@ -4,9 +4,20 @@ import matplotlib.pyplot as plt
 
 
 def get_kernel(x1, x2, kernel="linear", gamma=0.5):
+    """
+    기본 Linear kernel
+    
+    :param x1: 데이터 I 
+    :param x2:  데이터 J
+    :param kernel: Kernel 사용할 이름
+    :param gamma: 
+    :return: 
+    """
     if kernel == "linear":
+        # 선형 커널
         return np.dot(x1, x2)
     elif kernel == "rbf":
+        # Radial Basis Fuction 커널
         return np.exp(-gamma * np.linalg.norm(x1 - x2) ** 2)
     else :
         return np.dot(x1, x2)
@@ -16,21 +27,35 @@ class CustomSVM():
     SVM 구현하는 클래스
     """
     def __init__(self, X, y, C = 1, kernel="linear"):
+        """
+        
+        :param X: train 데이터 
+        :param y:  label 데이터
+        :param C: 파라미터
+        :param kernel: 커널 이름
+        """
         self.X = X                                  #X 데이터 저장
         self.N, self.M = X.shape                    #N, F 저장
-        self.y = y
+        self.y = y                                  # 정답 저장
         self.C = C
-        self.support_vector =None
-        self.w = None
-        self.b = None
+        self.support_vector =None                   # 분류기에 해당하는 값 (i 번째 샘플들)
+        self.w = None                               # Weight 가중치
+        self.b = None                               # Bias
 
         self.K = np.zeros((self.N, self.N))
+        # 커널 구현하는 함수 
         for i in range(self.N):
             for j in range(self.N):
                 self.K[i, j] = get_kernel(X[i], X[j])
 
 
     def get_constaint(self, tpye="non-linear"):
+        """
+        제약조건 설정하는 함수
+
+        :param tpye: linear or  non-linear(non-separating)
+        :return: 제약조건들
+        """
         n = self.N
 
         A = self.y.reshape(1, -1)
@@ -38,10 +63,16 @@ class CustomSVM():
 
 
         if tpye == "non-linear":
+            # diag 행렬 만들어서 0 보다 큰 조건, 그리고 C 보다 작은 조건 만들기
             G = np.concatenate([np.diag(np.ones(n) * -1), np.diag(np.ones(n))], axis=0)
+            
+            #H 값은 절반은 0보다 큰값 하나는 C 보다 작은값
             h = np.concatenate([np.zeros(n), np.ones(n) * self.C], axis=0)
         else:
+            
+            # diag 행렬 만들어서 0이상인 조건 만들기
             G = np.diag(np.ones(n) * -1)
+            # 무조건 0보다 큰값 조건
             h = (np.zeros(n))
 
 
@@ -50,7 +81,10 @@ class CustomSVM():
     def fit(self, option=True, type="non-linear", ):
         n = self.N
         diag_Y = np.diag(self.y.reshape(-1))
+        # H 행렬 만들기
         H = diag_Y @ self.K @ diag_Y
+        
+        
         P =  matrix(H)
         q = matrix(np.ones(n) * -1)
         G, h, A, b = self.get_constaint(type)
@@ -58,14 +92,17 @@ class CustomSVM():
         solvers.options['show_progress'] = option
         sol = solvers.qp(P, q, G, h, A, b)
 
+        # QP 함수의 결과를 받음
         alpha = sol['x']
         alpha = np.array(alpha)
 
         if type == "non-linear":
             print("non-linear 실행")
+            # non-separation 조건에서는 C보다는 작고 0보다는 커야하는 조건 달기
             zero_mask = ((alpha > 1e-4) & (alpha < self.C - 1e-4)).flatten()
         else:
             print("linear 실행")
+            # 무조건 0보다는 커야한다.
             zero_mask = (alpha > 1e-4).flatten()
 
         self.support_vector =self.X[zero_mask]
@@ -73,8 +110,10 @@ class CustomSVM():
         # 알파는 0 < 알파 < C 사이에 값을 구해야하기 때문에  mask 통해 특정 값만 추출
 
         new_X, new_Y, new_alpha = self.X[zero_mask], self.y[zero_mask], alpha[zero_mask]
+        # W 값 계하기 
         w = (new_alpha * new_Y.reshape(-1, 1) * new_X).sum(axis=0)
-
+        
+        # B 값 내적으로 가능하면, 평균값 구하기
         b = new_Y - np.dot(new_X, w)
         b= b.mean()
 
@@ -84,13 +123,22 @@ class CustomSVM():
         return 
     
     def decision_boundary(self, xx, f_x, f_y, b):
+        """
+        hyperplane 할때 사용하는 함수
+        """
         w = self.w
         return (-w[f_x] * xx - b) / w[f_y]
     
     def predict(self, X):
+        """
+        클래스 예측할때 사용하는 함수
+        """
         return np.sign(np.dot(X, self.w) + self.b)
     
     def score(self, X, y, verbose=False):
+        """
+        클래스 예측하고 정답을 얼마나 맞췄는지 알려주는 함수
+        """
         N = X.shape[0]
         result = (np.sum(self.predict(X) == y) / N) * 100
         if verbose:
@@ -98,6 +146,13 @@ class CustomSVM():
 
         return result
     def plot_svm(self, X, y, dims =[[0, 1]]):
+        """
+        2차원 feature 가지고 SVM 대한 hyperplane 그려주는 함수
+        :param X: 데이터 
+        :param y: 정답
+        :param dims: 차원
+        :return: 
+        """
         plt.figure(figsize=(20, 15))
 
         assert len(dims[0]) == 2 , "2개의 차원만 선택해 주세요"
